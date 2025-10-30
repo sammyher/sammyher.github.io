65f779d34ad8a1afcfb892b85ec8bc596e4764c3
sammyher <96896242+sammyher@users.noreply.github.com>
2025-10-30 21:17:56 +0000

    Deploying to gh-pages from @ sammyher/sammyher.github.io@b6a124a3bcbd0d0c3612148d9dad308159d66770 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,28 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-summer-corn-pasta",
+        },{id: "post-summer-corn-pasta",
         
           title: "(Summer) corn pasta",
         
